033deb6c12b156c79cc83787234799417df7e471
Paolo de Bruyn <mailingpaolo@gmail.com>
2020-10-13 20:29:56 +0200

    LogLocation Fix, locationAtTime, timesAtLocation

--- grove_gps.py
+++ grove_gps.py
@@ -122,7 +122,7 @@ class GPS_Extension:
     def __init__(self):
         g = GPS()
     
-    def getAddress(self):
+    def getCurrentAddress(self):
         global g
         
         g.refresh()
@@ -159,6 +159,16 @@ class GPS_Extension:
         b = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
         return 6371*b
     
+    def distanceToHome(self):
+        f = open("home.txt", "r")
+        home = f.readlines()[0]
+        f.close()
+        lat, lng = g.getCoordinatesFromAddress(home)
+        g.refresh()
+        lat2 = g.getLatitude()
+        lng2 = g.getLongitude()
+        return self.distance(lat, lng, lat2, lng2)
+    
     def setHomeAddress(self, address):
         f = open("home.txt", "w")
         f.write(home)
@@ -174,28 +184,31 @@ class GPS_Extension:
         dt = datetime.datetime.now()
         address1 = self.getAddress()
         h = open("history.txt", "a")
-        h.write(dt1 + "\n")
+        h.write(str(dt1) + "\n")
         h.write(address1 + "\n")
         h.close()
         while True:
             address2 = self.getAddress()
             if address1 != address2:
+                time_elapsed = time.time() - time_start
+                if time_elapsed > 600:
+                    favourites = open("favourites.txt")
+                    fav = json.load(favourites)
+                    favourites.close()
+                    if address2 in fav:
+                        fav[address2] += 1
+                    else:
+                        fav[address2] = 1
+                    new_favourites = open("favourites.txt", "w")
+                    json.dump(fav, new_favourites)
+                    new_favourites.close()
                 dt = datetime.datetime.now()
                 f = open("history.txt", "a")
-                f.write(dt + "\n")
+                f.write(str(dt) + "\n")
                 f.write(address2 + "\n")
                 f.close()
-                favourites = open("favourites.txt")
-                fav = json.load(favourites)
-                favourites.close()
-                if address2 in fav:
-                    fav[address2] += 1
-                else:
-                    fav[address2] = 1
-                new_favourites = open("favourites.txt", "w")
-                json.dump(fav, new_favourites)
-                new_favourites.close()
                 address1 = address2
+                time_start = time.time()
             time.sleep(60)
     
     def getFavouriteLocations(self):
@@ -213,8 +226,43 @@ class GPS_Extension:
             fav.pop(place)
             top.append(place)
         return top
+    
+    def locationAtTime(self, dt):
+        f = open("history.txt", "r")
+        data = f.readlines()
+        f.close()
+        start = 0
+        stop = len(data)
+        found = False
+        address = ""
+        while found == False:
+            pos = (stop - start)//2 + start
+            if pos%2 == 1:
+                pos-=1
+            if dt < data[pos][:19]:
+                stop = pos
+            else:
+                start = pos
+            if (stop - start) == 2:
+                found = True
+                address = data[pos+1]
+        return address
+    
+    def timesAtLocation(self, address):
+        favourites = open("favourites.txt")
+        fav = json.load(favourites)
+        favourites.close()
+        return fav[address]
+        
         
 if __name__ == "__main__":
     g = GPS_Extension()
     #print(g.getCoordinatesFromAddress("19 Loots Road Blairgowrie, Randburg"))
-    print(g.distance(-26.103062, 28.003815, -26.116043, 28.001292))
+    #print(g.distance(-26.103062, 28.003815, -26.116043, 28.001292))
+    #print(g.locationAtTime("2020-10-13 20:22:14.850905"))
+    #print(g.timesAtLocation("address3"))
+    
+#     f = open("history.txt", "a")
+#     f.write(str(datetime.datetime.now()) + "\n")
+#     f.write("9 Loots Road Blairgowrie, Randburg\n")
+#     f.close()
